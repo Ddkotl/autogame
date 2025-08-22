a5562a875bb33948825bdc1136ed834b9a568c4b
Dmitry KOTL <134155338+Ddkotl@users.noreply.github.com>
2025-08-22 09:31:52 +0000

    f

--- modules/get_resurses_to_squad.ts
+++ modules/get_resurses_to_squad.ts
@@ -9,7 +9,7 @@ export async function getResursesToSquad(page: Page) {
     await sleep(3000);
     await page.locator(".button_small").click();
     await sleep(3000);
-    await page.fill('input[name="gold"]', "100");
+    await page.fill('input[name="gold"]', "10");
     await page.locator(".button_small").click();
   } catch (error) {
     console.error("Не удалось пожертвовать в отряд", error);
