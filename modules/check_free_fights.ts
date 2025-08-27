f0da4a5224a33c039860188a49130e9eff9255b0
Dkotl <126883337+Dkotl@users.noreply.github.com>
2025-08-27 18:38:22 +0000

    f

--- modules/check_free_fights.ts
+++ modules/check_free_fights.ts
@@ -5,8 +5,8 @@ export async function checkFreeFights(page: Page) {
   try {
     await sleep(1000);
     const fights = await page
-      .locator('.footer_icons > span > a[href="/arena/main/?sorting=zombie"]')
-      .innerText();
+      .locator('.footer_icons  > span ').nth(8)
+      .innerText().then(data=>data.trim());
     await sleep(1000);
     return fights;
   } catch (error) {
