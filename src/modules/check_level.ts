343442635b380eeca1904cebc81d19564a7a9d42
Dkotl <126883337+Dkotl@users.noreply.github.com>
2025-08-30 18:39:01 +0000

    f

--- src/modules/check_level.ts
+++ src/modules/check_level.ts
@@ -3,7 +3,8 @@ import { sleep } from "../utils/sleep";
 
 export async function checkLevel(page: Page) {
   try {
-    await sleep(1000);
+    await page.goto("https://mvoo.ru",{waitUntil:"domcontentloaded"})
+    await sleep(3000);
     const level = await page
       .locator(".footer_icons  > span ")
       .nth(0)
