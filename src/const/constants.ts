62146cda1a9e3ec9a4841c95074bc4ad79327df1
Dkotl <126883337+Dkotl@users.noreply.github.com>
2025-09-08 18:08:29 +0000

    f

--- src/const/constants.ts
+++ src/const/constants.ts
@@ -1,5 +1,1 @@
-export const app_consts = {
-    php_session_id:"dac8081b678bd7c2abdc5950372948af",
-    user_agent:"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36",
-    base_sait_url:"https://mvoo.ru"
-}
+export const php_session_id = "dac8081b678bd7c2abdc5950372948af"
--- src/test.ts
+++ src/test.ts
@@ -1,21 +1,17 @@
 async function main() {
   await fetch("https://mvoo.ru/?take=true", {
-        headers: {
-              "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36",
-                    "Cookie": "PHPSESSID=dac8081b678bd7c2abdc5950372948af; SESSION_ID=d5bb55604e65f19f4f7f0b532c5dcc0f90caf98d89ed59c1bf8ae004779da3a4",
-                          "Referer": "https://mvoo.ru",
-                              }
-                                });
+    headers: {
+      Cookie:
+        "PHPSESSID=dac8081b678bd7c2abdc5950372948af; SESSION_ID=d5bb55604e65f19f4f7f0b532c5dcc0f90caf98d89ed59c1bf8ae004779da3a4",
+    },
+  });
   // 1. Получаем страницу боя
   const pageUrl =
     "https://mvoo.ru/arena/cache/attack/55314";
   const res = await fetch(pageUrl, {
     headers: {
-      "User-Agent":
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36",
       Cookie:
         "PHPSESSID=dac8081b678bd7c2abdc5950372948af; SESSION_ID=d5bb55604e65f19f4f7f0b532c5dcc0f90caf98d89ed59c1bf8ae004779da3a4",
-      Referer: "https://mvoo.ru/arena/",
     },
   });
 
@@ -36,10 +32,9 @@ async function main() {
   // 3. Делаем запрос по найденной ссылке
   const attackRes = await fetch(attackUrl, {
     headers: {
-      "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36",
-      "Cookie": "PHPSESSID=dac8081b678bd7c2abdc5950372948af; SESSION_ID=d5bb55604e65f19f4f7f0b532c5dcc0f90caf98d89ed59c1bf8ae004779da3a4",
-      "Referer": pageUrl,
-    }
+      Cookie:
+        "PHPSESSID=dac8081b678bd7c2abdc5950372948af; SESSION_ID=d5bb55604e65f19f4f7f0b532c5dcc0f90caf98d89ed59c1bf8ae004779da3a4",
+    },
   });
 
   const attackHtml = await attackRes.text();
