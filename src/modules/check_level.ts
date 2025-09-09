84d2273d7aba368f29cabb29cddfe870eae3f379
Dkotl <126883337+Dkotl@users.noreply.github.com>
2025-09-09 10:28:06 +0000

    f

--- src/modules/check_level.ts
+++ src/modules/check_level.ts
@@ -1,17 +1,8 @@
-import { Page } from "patchright";
-import { sleep } from "../utils/sleep";
 
-export async function checkLevel(page: Page) {
+
+export async function checkLevel(session_id:string) {
   try {
-    await page.goto("https://mvoo.ru", {
-      waitUntil: "domcontentloaded",
-    });
-    await sleep(3000);
-    const level = await page
-      .locator(".user_link")
-      .innerText();
-    await sleep(1000);
-    return +level.replace(/^.*\[(\d+)\]$/, "$1");
+    
   } catch (error) {
     console.error("Не удалось узнать уровень", error);
   }
--- src/test.ts
+++ src/test.ts
@@ -1,13 +1,6 @@
+
 async function main() {
-  await fetch("https://mvoo.ru/?take=true", {
-    headers: {
-      Cookie:
-        "PHPSESSID=dac8081b678bd7c2abdc5950372948af; SESSION_ID=d5bb55604e65f19f4f7f0b532c5dcc0f90caf98d89ed59c1bf8ae004779da3a4",
-    },
-  });
-  // 1. Получаем страницу боя
-  const pageUrl =
-    "https://mvoo.ru/arena/cache/attack/55314";
+  const pageUrl = "https://mvoo.ru";
   const res = await fetch(pageUrl, {
     headers: {
       Cookie:
@@ -17,28 +10,11 @@ async function main() {
 
   const html = await res.text();
   console.log(html);
+
   // 2. Находим ссылку на атаку в HTML
-  const match = html.match(
-    /href="(\/arena\/cache\/attack\/\d+\?cache=[^"]+)"/,
-  );
-  if (!match) {
-    console.error("Ссылка на атаку не найдена!");
-    return;
-  }
+  const match = html.match(/\[(\d+)\]/);
 
-  const attackUrl = "https://mvoo.ru" + match[1];
-  console.log("Нашли ссылку на атаку:", attackUrl);
-
-  // 3. Делаем запрос по найденной ссылке
-  const attackRes = await fetch(attackUrl, {
-    headers: {
-      Cookie:
-        "PHPSESSID=dac8081b678bd7c2abdc5950372948af; SESSION_ID=d5bb55604e65f19f4f7f0b532c5dcc0f90caf98d89ed59c1bf8ae004779da3a4",
-    },
-  });
-
-  const attackHtml = await attackRes.text();
-  console.log("HTML ответа на атаку:", attackHtml);
+  console.log(match[1]);
 }
 
 main();
