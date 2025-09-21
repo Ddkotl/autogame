e6960722cd36be370b26dacf33ef9cf8b84ba046
Dkotl <126883337+Dkotl@users.noreply.github.com>
2025-09-21 07:19:45 +0000

    send viruses

--- src/modules/get_prists_mana.ts
+++ src/modules/get_prists_mana.ts
@@ -2,11 +2,14 @@ import { php_session_id } from "../const/constants";
 
 export async function getPristsMana(session_id: string) {
   try {
-    await fetch("https://mvoo.ru/clan/castle/?priests=open&receive=true&confirm=true", {
-      headers: {
-        Cookie: `PHPSESSID=${php_session_id}; SESSION_ID=${session_id}`,
+    await fetch(
+      "https://mvoo.ru/clan/castle/?priests=open&receive=true&confirm=true",
+      {
+        headers: {
+          Cookie: `PHPSESSID=${php_session_id}; SESSION_ID=${session_id}`,
+        },
       },
-    });
+    );
   } catch (error) {
     console.log("не получилось получить ману жрецов");
   }
