36d22eb7235a70e7d710c284bed1deea063a76aa
Dkotl <126883337+Dkotl@users.noreply.github.com>
2025-09-22 07:59:46 +0000

    fix

--- src/const/constants.ts
+++ src/const/constants.ts
@@ -1,4 +1,4 @@
 export const php_session_id =
   "dac8081b678bd7c2abdc5950372948af";
 export const boss_id =
-  "5324591d3d9ff6c54cc2c7048c6360893e7566e335c30ea0617c82a70c516e2e";
+"1ba886dea7804b17c5f6b54645071c46224f78034ffddaded62a19c1a1897e96";
