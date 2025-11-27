b7e65f6006e80086010e98da4b4061a44e4e478e
Dmitry KOTL <134155338+Ddkotl@users.noreply.github.com>
2025-11-28 02:17:59 +0300

    Update start_feed.ts

--- src/mode/start_feed.ts
+++ src/mode/start_feed.ts
@@ -7,6 +7,7 @@ export async function startFeed(session_id: string) {
   //await getDayRevard(session_id)
   //const level = await checkLevel(session_id);
   //if (level >= 8) {
+   await sleep(2000);
   await feedMain(session_id, proxy);
   //}
 }
