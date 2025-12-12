321d1d1efc0124ac88e3206bb7b0c95178de408d
Dmitry KOTL <134155338+Ddkotl@users.noreply.github.com>
2025-12-12 08:02:05 +0300

    Update start_feed.ts

--- src/mode/start_feed.ts
+++ src/mode/start_feed.ts
@@ -1,6 +1,6 @@
 import { feedMain } from "../modules/feed_main";
 import { sleep } from "../utils/sleep";
-const proxy = true;
+const proxy = false;
 export async function startFeed(session_id: string) {
    await sleep(2000);
   await feedMain(session_id, proxy);
