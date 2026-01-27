b2f81285b86e1b1b49cbe2a119343082f1c84629
Dkotl <126883337+Dkotl@users.noreply.github.com>
2026-01-27 07:07:37 +0000

    f

--- src/boss_farm_glory.ts
+++ src/boss_farm_glory.ts
@@ -7,7 +7,7 @@ import fs from "fs";
 const sacrifice_list = JSON.parse(fs.readFileSync("./sacrifice_list.json", "utf-8"));
 //sacrifice_list.splice(0,101)
 (async () => {
-  for (let i = 0; i < 100; i++) {
+  for (let i = 0; i < 120; i++) {
     try {
       await atackPlayer(boss_id, sacrifice_list[i]);
       await mineGold(boss_id);
