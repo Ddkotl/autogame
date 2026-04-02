eafd524a6b97a26f9f1d6e2c0972699e8a31a846
Dmitry KOTL <134155338+Ddkotl@users.noreply.github.com>
2026-04-02 20:21:55 +0300

    Update constants.ts

--- src/const/constants.ts
+++ src/const/constants.ts
@@ -1,4 +1,4 @@
 export const php_session_id =
   "dac8081b678bd7c2abdc5950372948af";
 export const boss_id =
-  "44f95a5f961a1e79482a0e9be837baff809d27fcc02ba17bb0b953fc9858de2a";
+  "4485882065b50c20960f02fce66cad45d99591c40d87ef98fde8857f60db08df";
